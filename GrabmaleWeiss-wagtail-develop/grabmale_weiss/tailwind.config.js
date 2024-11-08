d67fe7c163315b208f7d8b85040c822869468250
Viktor Rura <ruravictor@gmail.com>
2024-11-08 19:50:40 +0000

    Update the home page

--- GrabmaleWeiss-wagtail-develop/grabmale_weiss/tailwind.config.js
+++ GrabmaleWeiss-wagtail-develop/grabmale_weiss/tailwind.config.js
@@ -2,12 +2,10 @@
 module.exports = {
   content: ["**/templates/**/*.html"],
   theme: {
-    screens: {
-      lg: { max: "1999.99px" },
-      md: { max: "991.99px" },
-      sm: { max: "479.99px" },
-    },
     extend: {
+      minHeight: {
+        "100svh": "90svh", // This sets a custom min-height with 100 small viewport height
+      },
       fontSize: {
         "h1-desktop": ["72px", { lineHeight: "120%" }],
         "h1-mobile": ["36px", { lineHeight: "120%" }],
@@ -38,8 +36,7 @@ module.exports = {
         mediumgrey: "#E1E1E1",
         darkgrey: "#636363",
         black: "#000000",
-        gradient:
-          "linear-gradient(90deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.10) 44.88%)",
+        gradient: "rgba(237, 237, 237, 0.5)",
       },
     },
   },
